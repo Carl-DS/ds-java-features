c71510fa4ddc5fe9fa2703f9fbc7055b27808601
Carl-DS <ldscode@163.com>
2018-09-30 14:28:57 +0800

    feat: AIO 创建的TimeClient

---
+++ src/main/java/nettyguide/c24/ReadCompletionHandler.java
@@ -0,0 +1,80 @@
+package nettyguide.c24;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.Date;
+
+/**
+ * @author duosheng
+ * @since 2018/9/30
+ */
+public class ReadCompletionHandler implements CompletionHandler<Integer, ByteBuffer> {
+
+    private AsynchronousSocketChannel channel;
+
+    public ReadCompletionHandler(AsynchronousSocketChannel channel) {
+        if (this.channel == null) {
+            this.channel = channel;
+        }
+    }
+
+    @Override
+    public void completed(Integer result, ByteBuffer attachment) {
+        attachment.flip();
+        byte[] body = new byte[attachment.remaining()];
+        attachment.get(body);
+        try {
+            String req = new String(body, "UTF-8");
+            System.out.println("The time server receive order : " + req);
+            String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(req) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+            doWrite(currentTime);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    /**
+     * 将当前系统时间发送给客户端
+     * @param currentTime
+     */
+    public void doWrite(String currentTime) {
+        if (currentTime != null && currentTime.trim().length() > 0) {
+            byte[] bytes = currentTime.getBytes();
+            ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+            writeBuffer.put(bytes);
+            // flip 操作，为后续从缓冲区读取数据做准备
+            writeBuffer.flip();
+            channel.write(writeBuffer, writeBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override
+                public void completed(Integer result, ByteBuffer attachment) {
+                    // 如果没有发送完成，继续发送
+                    if (attachment.hasRemaining()) {
+                        channel.write(attachment, attachment, this);
+                    }
+                }
+
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    try {
+                        channel.close();
+                    } catch (IOException e) {
+                        // ignore on close
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void failed(Throwable exc, ByteBuffer attachment) {
+        try {
+            this.channel.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/nettyguide/c24/TimeServer.java
@@ -0,0 +1,23 @@
+package nettyguide.c24;
+
+/**
+ * AIO TimeServer
+ * @author duosheng
+ * @since 2018/9/30
+ */
+public class TimeServer {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+
+        AsyncTimeServerHandler timeServer = new AsyncTimeServerHandler(port);
+        new Thread(timeServer, "AIO-AsyncTimeServerHandler-001").start();
+    }
+}
