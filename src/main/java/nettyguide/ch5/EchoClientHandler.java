6942a8c01de4f2fbc666044f438e352ae3a1ebf2
Carl-DS <ldscode@163.com>
2018-10-08 14:56:11 +0800

    feat: ch5 分隔符与定长解码器的应用

---
+++ src/main/java/nettyguide/ch5/EchoClientHandler.java
@@ -0,0 +1,43 @@
+package nettyguide.ch5;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author duosheng
+ * @since 2018/10/8
+ */
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+
+    private int counter;
+
+    static final String ECHO_REQ = "Hi, Welcome to Netty.$_";
+
+    public EchoClientHandler() {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("This is " + ++counter + "times receive server : [" + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
